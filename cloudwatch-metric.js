79c095835c16bccda74342c1369f85bb56775ebd
SmitPatel-31 <patel.smit3131@gmail.com>
2025-03-26 20:50:46 -0400

    code updated

--- cloudwatch-metric.js
+++ cloudwatch-metric.js
@@ -2,6 +2,11 @@ const AWS = require("aws-sdk");
 const cloudwatch = new AWS.CloudWatch();
 
 function logMetric(metricName, apiName, value) {
+  let unit = "Milliseconds"; // Default to timing metrics
+
+  if (metricName.includes("Count")) {
+    unit = "Count";
+  }
   const params = {
     MetricData: [
       {
